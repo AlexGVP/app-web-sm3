bbb77b1bcc00a9d93ab6a91fbda17648c23df8ea
AlexGVP <i202122156@cibertec.edu.pe>
2023-09-10 10:30:18 -0500

    confirmar cambios

---
+++ src/main/java/cibertec/edu/pe/appwebsm3/AppWebSm3Application.java
@@ -0,0 +1,13 @@
+package cibertec.edu.pe.appwebsm3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppWebSm3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppWebSm3Application.class, args);
+	}
+
+}
---
+++ src/test/java/cibertec/edu/pe/appwebsm3/AppWebSm3ApplicationTests.java
@@ -0,0 +1,13 @@
+package cibertec.edu.pe.appwebsm3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppWebSm3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
